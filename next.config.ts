cf5fd7cec4573d7f585cd421eb581ad7bfb6a38b
Ni Ngo <ni.ngo@asnet.com.vn>
2026-01-02 16:23:14 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   serverExternalPackages: ['@remotion/renderer'],
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
