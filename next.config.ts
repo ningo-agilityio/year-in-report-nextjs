0cbce4ac41d55b0b2f5388cb5e5443f10c30dfee
Ni Ngo <ni.ngo@asnet.com.vn>
2026-01-02 16:30:02 +0700

    update next config with node_modules read path

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
+console.log("===current path===", path.join(__dirname))
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   serverExternalPackages: ['@remotion/renderer'],
   outputFileTracingRoot: path.join(__dirname),
+  outputFileTracingIncludes: {
+    '/api/renderers': ['./node_modules/.remotion'],
+  },
 };
 
 export default nextConfig;
